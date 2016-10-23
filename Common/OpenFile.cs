f84bcedde2e223068b390c73977a35a530be1f82
PeterShen <petershen0307@hotmail.com>
2016-10-23 10:30:15 +0800

    refactor project structure move OpenFile to Common project

---
+++ Common/OpenFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Common
+{
+    public class OpenFile
+    {
+        public static void ReadSettings(Dictionary<string, string> settingDict)
+        {
+            string inputStr = "";
+            using (FileStream fs = new FileStream("pipe.config", FileMode.Open))
+            {
+                int a = -1;
+                while ((a = fs.ReadByte()) != -1)
+                {
+                    inputStr += Convert.ToChar(a);
+                }
+            }
+            string[] splitKey = new string[] { System.Environment.NewLine };
+            string[] settings = inputStr.Split(splitKey, StringSplitOptions.None);
+            foreach (var i in settings)
+            {
+                string[] value = i.Split('=');
+                settingDict[value[0]] = value[1];
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- named_pipe_client/Clinet.cs
+++ named_pipe_client/Clinet.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.IO.Pipes;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Common;
 
 namespace named_pipe_client
 {
@@ -8,37 +11,13 @@ namespace named_pipe_client
     {
         static void Main(string[] args)
         {
-            string serverName = "", pipeName = "";
-            ReadSettings(ref serverName, ref pipeName);
-            PipeClient(serverName, pipeName);
-        }
-
-        static void ReadSettings(ref string serverName, ref string pipeName)
-        {
-            string inputStr = "";
-            using (FileStream fs = new FileStream("pipe.config", FileMode.Open))
-            {
-                int a = -1;
-                while ((a = fs.ReadByte()) != -1)
-                {
-                    inputStr += Convert.ToChar(a);
-                }
-            }
-            string[] splitKey = new string[] { System.Environment.NewLine };
-            string[] settings = inputStr.Split(splitKey, StringSplitOptions.None);
-            foreach (var i in settings)
-            {
-                string[] value = i.Split('=');
-                if ("serverName" == value[0])
-                {
-                    serverName = value[1];
-                }
-                else if ("pipeName" == value[0])
-                {
-                    pipeName = value[1];
-                }
-                Console.WriteLine(i);
-            }
+            Dictionary<string, string> settingDict = new Dictionary<string, string>();
+            OpenFile.ReadSettings(settingDict);
+            const string serverName = "serverName";
+            const string pipeName = "pipeName";
+            Debug.Assert(settingDict.ContainsKey(serverName));
+            Debug.Assert(settingDict.ContainsKey(pipeName));
+            PipeClient(settingDict[serverName], settingDict[pipeName]);
         }
 
         static void PipeClient(string serverName, string pipeName)
